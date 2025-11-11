e8103ac98830f500151de430ffe6429b92b0b31d
Laura Klusendorf <laurafklusendorf@gmail.com>
2025-11-11 10:55:44 +0000

    send off

--- course/exercises/exercises_1.py
+++ course/exercises/exercises_1.py
@@ -1,48 +1,87 @@
+
+###"""Return the sum of all elements in the list 'numbers'."""
 def sum_list(numbers):
-    """Return the sum of all elements in the list 'numbers'."""
-    return 0
+    x=sum(numbers)
+    return x
+numbers= [1,2,3,4,5]
+sum_list(numbers)
 
 
+###    """Return the first element of the tuple 't'."""
 def first_of_tuple(t):
-    """Return the first element of the tuple 't'."""
-    return 0
+    first=t[0]
+    return first
+mytuple=("Inky", "Pinky", "Blinky", "Clyde")
+first_of_tuple(mytuple)
 
 
+###"""Return True if 'key' exists in dictionary 'd', else False."""
 def has_key(d, key):
-    """Return True if 'key' exists in dictionary 'd', else False."""
-    return 0
+    if key in d:
+      return True
+    else:
+      return False
+    
+key = 4
+d= [1,2,3,5]
+has_key(d, key)
 
-
+###    """Round the float 'f' to 2 decimal places."""
 def round_float(f):
-    """Round the float 'f' to 2 decimal places."""
-    return 0
+    rounded=round(f, 2)
+    return rounded
+f=2.32623
+round_float(f)
 
 
+###    """Return a new list that is the reverse of 'lst'."""
 def reverse_list(lst):
-    """Return a new list that is the reverse of 'lst'."""
-    return 0
+    reverse=lst[::-1]
+    return reverse
+lst=[1,2,3,4,5]
+reverse_list(lst)
 
 
+### """For a list of items 'lst', count how many times element 'item' occurs."""
 def count_occurrences(lst, item):
-    """For a list of items 'lst', count how many times element 'item' occurs."""
-    return 0
+    count=lst.count(item)
+    return count
+lst=[1,1,1,2,2,3,3,5,6,7,7]
+item=1
+count_occurrences(lst, item)
 
 
+###  """Convert a list of (key, value) tuples 'pairs' into a dictionary."""
 def tuples_to_dict(pairs):
-    """Convert a list of (key, value) tuples 'pairs' into a dictionary."""
-    return 0
+    dictionary=dict(pairs)
+    return dictionary
+  
+pairs = [('a',1),('b',2),('c',3)]
+tuples_to_dict(pairs)
 
-
+###    """Return the number of characters in string 's'."""
 def string_length(s):
-    """Return the number of characters in string 's'."""
-    return 0
+    length=len(s)
+    return length
+s=("Laura Klusendorf")
+string_length(s)
 
 
+###    """Return a list of unique elements from 'lst'."""
 def unique_elements(lst):
-    """Return a list of unique elements from 'lst'."""
-    return 0
+    unique = list(set(lst))
+    return unique
+  
+lst=[1,1,1,2,3,4,5,5,5,6,6]
+unique_elements(lst)
 
 
+###    """Return a new dictionary with keys and values of 'd' swapped."""
 def swap_dict(d):
-    """Return a new dictionary with keys and values of 'd' swapped."""
-    return 0
+    swap={value: key for key, value in d.items()}
+    return swap
+
+d={'a':1, 'b':2, 'c':3}
+swap_dict(d)
+
+
