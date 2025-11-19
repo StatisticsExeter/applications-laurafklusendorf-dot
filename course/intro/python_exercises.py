d017c436181ad6358424f8ad8dd8af52fe668e02
Laura Klusendorf <laurafklusendorf@gamil.com>
2025-11-19 14:27:06 +0000

    filled out

--- course/intro/python_exercises.py
+++ course/intro/python_exercises.py
@@ -7,54 +7,57 @@ def sum_list(numbers):
 def max_value(numbers):
     """Given a list of numbers 'numbers'
     return the maximum value of this list."""
-    
-    return 0
+    return max(numbers)
 
 
 def reverse_string(s):
     """Given a string 'string'
     return the reversed version of the input string."""
-    
-    return 0
+    return s[::-1]
 
 
 def filter_even(numbers):
     """Given a list of numbers 'numbers'
     return a list containing only the even numbers from the input list."""
-    return 0
+    even = list(filter(lambda num: num % 2 == 0, numbers))
+    return even
 
 
 def get_fifth_row(df):
     """Given a dataframe 'df'
     return the fifth row of this as a pandas DataFrame."""
-    return 0
+    return df.iloc[4]
 
 
 def column_mean(df, column):
     """Given a dataframe 'df' and the name of a column 'column'
     return the mean of the specified column in a pandas DataFrame."""
-    return 0
+    return df[column]
 
 
 def lookup_key(d, key):
     """Given a dictionary 'd' and a key 'key'
     return the value associated with the key in the dictionary."""
-    return 0
+    value = d[key]
+    return value
 
 
 def count_occurrences(lst):
     """Given a list 'lst'
     return a dictionary with counts of each unique element in the list."""
-    return 0
+    freq = Counter(lst)
+    return freq
 
 
 def drop_missing(df):
     """Given a dataframe 'df' with some rows containing missing values,
     return a DataFrame with rows containing missing values removed."""
-    return 0
+    df_new = df.dropna()
+    return df_new
 
 
 def value_counts_df(df, column):
     """Given a dataframe 'df' with various columns and the name of one of those columns 'column',
     return a DataFrame with value counts of the specified column."""
-     return 0
+    count = df.count(column)
+    return count
