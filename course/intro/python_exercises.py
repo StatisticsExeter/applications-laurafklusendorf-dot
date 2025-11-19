8ee368c31f8c99861f0ce74eb2b73e78e793eb32
Laura Klusendorf <laurafklusendorf@gamil.com>
2025-11-19 14:11:43 +0000

    sum function

---
+++ course/intro/python_exercises.py
@@ -0,0 +1,60 @@
+def sum_list(numbers):
+    """Given a list of integers 'numbers'
+    return the sum of this list."""
+    return sum(numbers)
+
+
+def max_value(numbers):
+    """Given a list of numbers 'numbers'
+    return the maximum value of this list."""
+    
+    return 0
+
+
+def reverse_string(s):
+    """Given a string 'string'
+    return the reversed version of the input string."""
+    
+    return 0
+
+
+def filter_even(numbers):
+    """Given a list of numbers 'numbers'
+    return a list containing only the even numbers from the input list."""
+    return 0
+
+
+def get_fifth_row(df):
+    """Given a dataframe 'df'
+    return the fifth row of this as a pandas DataFrame."""
+    return 0
+
+
+def column_mean(df, column):
+    """Given a dataframe 'df' and the name of a column 'column'
+    return the mean of the specified column in a pandas DataFrame."""
+    return 0
+
+
+def lookup_key(d, key):
+    """Given a dictionary 'd' and a key 'key'
+    return the value associated with the key in the dictionary."""
+    return 0
+
+
+def count_occurrences(lst):
+    """Given a list 'lst'
+    return a dictionary with counts of each unique element in the list."""
+    return 0
+
+
+def drop_missing(df):
+    """Given a dataframe 'df' with some rows containing missing values,
+    return a DataFrame with rows containing missing values removed."""
+    return 0
+
+
+def value_counts_df(df, column):
+    """Given a dataframe 'df' with various columns and the name of one of those columns 'column',
+    return a DataFrame with value counts of the specified column."""
+     return 0
