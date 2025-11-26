509f9426f7d76203d66043b18dca29bfa3044ac4
Laura Klusendorf <laurafklusendorf@gamil.com>
2025-11-26 21:17:10 +0000

    fixed it so the test works

--- course/supervised_classification/predict.py
+++ course/supervised_classification/predict.py
@@ -3,11 +3,11 @@ import pandas as pd
 from course.utils import find_project_root
 
 
-def predict(model_path, X_test_path, y_pred_path):
+def predict(model_path, X_test_path, y_pred_path, y_pred_prob_path):
     model = joblib.load(model_path)
     X_test = pd.read_csv(X_test_path)
     """Form an object y_pred containing a list of your classifer predictions"""
-    y_pred = 
+    y_pred = model.predict(X_test)
     y_pred_series = pd.Series(y_pred, name='predicted_built_age')
     y_pred_series.to_csv(y_pred_path, index=False)
 
